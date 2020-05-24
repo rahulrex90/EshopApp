129daac3ea880b772aa814d6fe829c7f206183d1
rahulrex90 <rahulrex90@gmail.com>
2020-05-24 23:25:18 +0530

    test

--- src/app/products/products.routing.ts
+++ src/app/products/products.routing.ts
@@ -4,7 +4,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { AuthGuardService } from '../guards/auth-guard.service';
 
-
+// Const Routes
 const routes: Routes = [
     {path: '', component: ProductListComponent, canActivate : [AuthGuardService]},
     {path: 'product-list', component : ProductListComponent, canActivate : [AuthGuardService]},
